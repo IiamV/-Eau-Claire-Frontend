8c9c138f89aa3b3713ef39ce767f154ebc89da5f
uselessV <77937873+IiamV@users.noreply.github.com>
2025-10-19 08:16:40 +0700

    Added "environments" folder and apply it to "angular.json", updated "package-lock".

--- eau-claire-farm-management/src/app/app.routes.ts
+++ eau-claire-farm-management/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/containters/login/login';
-import { ForgetPassword } from './auth/containters/forget-password/forget-password';
+import { ForgetPassword } from './auth/containters/forgot-password/forgot-password';
 import { OtpVerification } from './auth/containters/otp-verification/otp-verification';
 
 export const routes: Routes = [
@@ -9,7 +9,7 @@ export const routes: Routes = [
 
     // Định nghĩa các trang trong luồng Authentication (đăng nhập, đăng ký, quên mật khẩu, OTP)
     { path: 'login', component: Login },
-    { path: 'forget-password', component: ForgetPassword },
+    { path: 'forgot-password', component: ForgetPassword },
     { path: 'otp-verification', component: OtpVerification },
 
     // Trang sau khi đăng nhập
